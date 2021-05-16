054d21193fc79d477fd7a4368b5a08fda2c86b37
vmacchione <vicente.macchione1@gmail.com>
2021-05-16 20:48:13 -0300

    se genera la estructura del servidor y se establece la carpeta estatica public

--- webDesign/app.js
+++ webDesign/app.js
@@ -1,2 +1,26 @@
-// Requerir expres 
-// Requerir path
+//Requerir express
+const express = require("express");
+const app = express();
+
+//Requiriendo Path y carpeta estatica
+const path = require("path");
+const publicPath = path.resolve(__dirname,"./public");
+app.use(express.static(publicPath));
+
+//Levantando server
+app.listen(3030,() => {
+    console.log("Server corriendo en el local host 3030")
+});
+
+//Creando la ruta para mostrar el contenido en el navegador
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+});
+
+
+
+
+
+
+
+

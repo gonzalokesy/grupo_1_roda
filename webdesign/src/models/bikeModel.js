0afbf10f12fa851df7cd4b9fba3d81d804b8628e
andreaeichenberger <andreaeichenberger.ae@gmail.com>
2021-07-03 14:13:31 -0300

    Primer avance con models

--- webdesign/src/models/bikeModel.js
+++ webdesign/src/models/bikeModel.js
@@ -1,3 +1,26 @@
 const path = require("path");
 const fs = require("fs");
 
+// Ubicación del archivo bikes.json en /data
+const directory = path.resolve(__dirname, "../data/bikes.json");
+
+const bikeModel = {
+    directory: path.resolve(__dirname, "../data/bikes.json"),
+    all: function() {
+        const readFile = fs.readFileSync(directory, "utf-8");
+        const convertFile = JSON.parse(readFile);
+        return convertFile;
+    },
+    new: function(data, file) {
+        let bikes = this.all();
+        let newBike = {
+            id: bikes.length > 0 ? bikes[bikes.length - 1].id + 1 : 1,
+            name: data.name,
+            description: data.description,
+            image: file.filename,
+            price: data.price,
+        }
+    }
+}
+
+module.exports = bikeModel;

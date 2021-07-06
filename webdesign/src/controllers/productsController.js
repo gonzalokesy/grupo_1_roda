40e1c99e7fb3baa0578021a038bf411d6e5b930d
andreaeichenberger <andreaeichenberger.ae@gmail.com>
2021-07-06 14:57:12 -0300

    Form de edición en funcionamiento

--- webdesign/src/controllers/productsController.js
+++ webdesign/src/controllers/productsController.js
@@ -1,13 +1,13 @@
 // Requerimos modelos
-const bikeModel = require('../models/bikeModel');
-const accessoryModel = require('../models/accessoryModel');
+const bikeModel = require("../models/bikeModel");
+const accessoryModel = require("../models/accessoryModel");
 
 const productsController = {
     indexBikes: (req, res) => {
         return res.render("products/product-listBike");
     },
     indexAccessories: (req, res) => {
-        return res.render("products/product-listAccessories");   
+        return res.render("products/product-listAccessories");
     },
     showBike: (req, res) => {
         return res.render("products/product-descriptionBike");
@@ -21,27 +21,38 @@ const productsController = {
     save: (req, res) => {
         if (req.body.category == "bike") {
             let result = bikeModel.new(req.body, req.files);
-        return result == true ? res.redirect("/") : res.send("Error al cargar la informacion");
+            return result == true
+                ? res.redirect("/")
+                : res.send("Error al cargar la informacion");
         } else if (req.body.category == "accessory") {
             let result = accessoryModel.new(req.body, req.files);
-        return result == true ? res.redirect("/") : res.send("Error al cargar la informacion");
+            return result == true
+                ? res.redirect("/")
+                : res.send("Error al cargar la informacion");
         }
     },
     edit: (req, res) => {
-        return res.render("products/edit", {bikes:bikeModel.one(req.params.id), accessories:accessoryModel.one(req.params.id)});
+        return res.render("products/edit", {
+            product: bikeModel.one(req.params.id) ? bikeModel.one(req.params.id) : accessoryModel.one(req.params.id),
+        });
     },
     update: (req, res) => {
+        //return res.send({body:req.body, files:req.files, id:req.params.id})
         if (req.body.category == "bike") {
             let result = bikeModel.edit(req.body, req.files, req.params.id);
-        return result == true ? res.redirect("/") : res.send("Error al cargar la informacion");
+            return result == true
+                ? res.redirect("/")
+                : res.send("Error al cargar la informacion");
         } else if (req.body.category == "accessory") {
             let result = accessoryModel.edit(req.body, req.files, req.params.id);
-        return result == true ? res.redirect("/") : res.send("Error al cargar la informacion");
+            return result == true
+                ? res.redirect("/")
+                : res.send("Error al cargar la informacion");
         }
     },
     delete: (req, res) => {
         //Falta desarrollar para capturar la información con el req.body
-    }
+    },
 };
 
 module.exports = productsController;

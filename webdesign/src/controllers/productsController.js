39305508911e631395517a1eab855f4473934bb7
andreaeichenberger <andreaeichenberger.ae@gmail.com>
2021-07-08 13:04:03 -0300

    Paso de productos de un json a otro json

--- webdesign/src/controllers/productsController.js
+++ webdesign/src/controllers/productsController.js
@@ -4,16 +4,24 @@ const accessoryModel = require("../models/accessoryModel");
 
 const productsController = {
     indexBikes: (req, res) => {
-        return res.render("products/product-listBike", {bikes: bikeModel.allBikes()});
+        return res.render("products/product-listBike", {
+            bikes: bikeModel.allBikes(),
+        });
     },
     indexAccessories: (req, res) => {
-        return res.render("products/product-listAccessories", {accessories: accessoryModel.allAccessories()});
+        return res.render("products/product-listAccessories", {
+            accessories: accessoryModel.allAccessories(),
+        });
     },
     showBike: (req, res) => {
-        return res.render("products/product-descriptionBike", {bikes: bikeModel.one(req.params.id)});
+        return res.render("products/product-descriptionBike", {
+            bikes: bikeModel.one(req.params.id),
+        });
     },
     showAccessory: (req, res) => {
-        return res.render("products/product-descriptionAccessories", {accessories: accessoryModel.one(req.params.id)});
+        return res.render("products/product-descriptionAccessories", {
+            accessories: accessoryModel.one(req.params.id),
+        });
     },
     create: (req, res) => {
         return res.render("products/create");
@@ -33,28 +41,55 @@ const productsController = {
     },
     edit: (req, res) => {
         return res.render("products/edit", {
-            product: bikeModel.one(req.params.id) ? bikeModel.one(req.params.id) : accessoryModel.one(req.params.id),
+            product: bikeModel.one(req.params.id)
+                ? bikeModel.one(req.params.id)
+                : accessoryModel.one(req.params.id),
         });
     },
     update: (req, res) => {
-        //return res.send({body:req.body, files:req.files, id:req.params.id})
-        if (req.body.category == "bike") {
-            let result = bikeModel.edit(req.body, req.files, req.params.id);
-            return result == true ? res.redirect("/"): res.send("Error al cargar la informacion");
-        } else if (req.body.category == "accessory") {
-            let result = accessoryModel.edit(req.body, req.files, req.params.id);
-            return result == true ? res.redirect("/") : res.send("Error al cargar la informacion");
+        if (bikeModel.one(req.params.id)) {
+            //si llega ID valido category "bike" y ejecuto bikeModel
+            if (req.body.category == "bike") {
+                let result = bikeModel.edit(req.body, req.files, req.params.id);
+                return result == true
+                    ? res.redirect("/")
+                    : res.send("Error al cargar la informacion");
+            }
+            //si llega ID pero con categoria "accessory", creo un nuevo producto y elimino el que esta
+            else if (req.body.category == "accessory") {
+                bikeModel.delete(req.params.id);
+                let result = accessoryModel.new(req.body, req.files);
+                return result == true
+                    ? res.redirect("/")
+                    : res.send("Error al cargar la informacion");
+            }
+        } else if (accessoryModel.one(req.params.id)) {
+            //si llega ID valido category "accessory" ejecuto accessoryModel
+            if (req.body.category == "accessory") {
+                let result = accessoryModel.edit(req.body, req.files, req.params.id);
+                return result == true
+                    ? res.redirect("/")
+                    : res.send("Error al cargar la informacion");
+            }
+            //si llega ID pero con categoria "accessory", creo un nuevo producto y elimino el que esta
+            else if (req.body.category == "bike") {
+                accessoryModel.delete(req.params.id);
+                let result = bikeModel.new(req.body, req.files);
+                return result == true
+                    ? res.redirect("/")
+                    : res.send("Error al cargar la informacion");
+            }
         }
     },
     delete: (req, res) => {
-        if (bikeModel.one (req.params.id)) {
-            bikeModel.delete (req.params.id)
-            return res.redirect ("/")
-        }else{
-            accessoryModel.delete (req.params.id)
-            return res.redirect ("/")
+        if (bikeModel.one(req.params.id)) {
+            bikeModel.delete(req.params.id);
+            return res.redirect("/");
+        } else {
+            accessoryModel.delete(req.params.id);
+            return res.redirect("/");
         }
-    }
-}
+    },
+};
 
 module.exports = productsController;

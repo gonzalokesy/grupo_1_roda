71607a2b031ac5f301d0f8c9f8380ce1aa48bcec
gonzalokesy <gonzalo.kesy@gmail.com>
2021-09-11 11:05:16 -0300

    Valdiaciones Front

--- webdesign/public/validFront/validFrontLogin.js
+++ webdesign/public/validFront/validFrontLogin.js
@@ -1,24 +1,29 @@
-window.addEventListener('load', function() {
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+};
 
-    //Elements
-    let buttonSubmit = document.querySelector(".submit");
-    let email = document.querySelector(".email");
-    
-    //ButtonSubmit
-    buttonSubmit.addEventListener('click', function(event){
-    event.preventDefault();
-    let errors = {}
 
-    if(email.value.length < 1){
-    errors.name = 'Debes completar con tu email'
+window.addEventListener("load", function(){ //Inicio de la carga del sitio
+    const form = document.querySelector("form.form")
+    const inputs = document.querySelectorAll(".form input")
+
+        form.addEventListener ("submit", function(e){
+        e.preventDefault();
+    });
+
+    const validarFormulario = () => {
+        console.log('ejecutado');
     }
 
-    if(Object.keys(errores).length >= 1){
+    inputs.forEach((input) => {
+        input.addEventListener('keyup', validarFormulario); 
+        input.addEventListener('blur', validarFormulario); 
+    });
 
-    erName.innerText = (errores.name) ? errores.name : '';
-    } else {
 
-    registerForm.submit();
-    }
-    })
-    })
+
+});//Cierre de addEventListener de carga de sitio
